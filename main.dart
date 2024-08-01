87225d5f49b15c48963646ae6fcbb4b66511470e
unknown <mayllamacetko13@gmail.com>
2024-07-31 21:32:57 -0300

    att

---
+++ main.dart
@@ -0,0 +1,6 @@
+void main() {
+  int a = 1;
+  print("O número printado é:" + a.toString()); //o + é utilizado para concatenar//
+}
+//para rodar o código é só esrever no terminal: dart main.dart//
+// É necessário uma main para funcionar//
